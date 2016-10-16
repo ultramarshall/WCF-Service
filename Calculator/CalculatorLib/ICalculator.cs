882870ea565a1a4a8c9fb3c1f59d5177f0ca2bf8
Marshall Telaumbanua <ugd24jam@gmail.com>
2016-10-16 23:27:19 +0700

    WCF Service - C#

---
+++ Calculator/CalculatorLib/ICalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+
+namespace CalculatorLib
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        float penambahan(float a, float b);
+        [OperationContract]
+        float pengurangan(float a, float b);
+        [OperationContract]
+        float perkalian(float a, float b);
+        [OperationContract]
+        float pembagian(float a, float b);
+
+    }
+}
---
+++ Calculator/CalculatorServer/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//menambahkan CalculatorLib
+using CalculatorLib;
+
+// 
+using System.ServiceModel;
+
+namespace CalculatorServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.PerCall)]
+    class Calculator : ICalculator
+    {
+        public float penambahan(float a, float b)
+        {
+            return a + b;
+        }
+
+        public float pengurangan(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float perkalian(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float pembagian(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
